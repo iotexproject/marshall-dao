264c9006934d710720e4b3c7e6e6534051830fa9
ququzone <xueping.yang@gmail.com>
2024-01-15 16:25:33 +0800

    deploy gov

---
+++ script/deploy_gov.ts
@@ -0,0 +1,16 @@
+import { ethers } from 'hardhat';
+
+async function main() {
+  if (!process.env.VE) {
+    console.log(`Please provide VE address`);
+    return;
+  }
+
+  const governor = await ethers.deployContract('MarshallGovernor', [process.env.VE]);
+  console.log(`MarshallGovernor deployed to ${governor.target}`);
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exitCode = 1;
+});
