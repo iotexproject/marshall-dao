91c08a53f06babf885ed50241012f798b3deb8d7
ququzone <xueping.yang@gmail.com>
2024-01-28 19:06:40 +0800

    add two node to merkle tree

--- test/VotingEscrow.test.ts
+++ test/VotingEscrow.test.ts
@@ -24,32 +24,53 @@ describe('VotingEscrow', function () {
     });
   });
 
-  beforeEach(function () {
-    now = Math.floor(new Date().getTime() / 1000);
-  });
+  describe('native bucket', function () {
+    let root: string;
+    let node1: string;
+    let node2: string;
 
-  it('Native bucket Mapping', async function () {
-    const bucketId = 200;
-    const voter = accounts[1].address;
-    const end = now + YEAR;
-    const stakedAmount = ethers.parseEther('100');
-    const root = ethers.keccak256(
-      ethers.solidityPacked(['uint256', 'address', 'uint256', 'uint256'], [bucketId, voter, end, stakedAmount]),
-    );
+    before(function () {
+      now = Math.floor(new Date().getTime() / 1000);
 
-    await expect(ve.connect(accounts[1]).commitNativeRoot(root)).to.be.revertedWithCustomError(ve, 'NotTeam');
-    await ve.connect(accounts[0]).commitNativeRoot(root);
-    await ve.connect(accounts[0]).approveNativeRoot();
+      node1 = ethers.keccak256(
+        ethers.solidityPacked(
+          ['uint256', 'address', 'uint256', 'uint256'],
+          [200, accounts[1].address, now + YEAR, ethers.parseEther('100')],
+        ),
+      );
 
-    expect(await ve.nativeRoot()).to.equal(root);
+      node2 = ethers.keccak256(
+        ethers.solidityPacked(
+          ['uint256', 'address', 'uint256', 'uint256'],
+          [201, accounts[2].address, now + WEEK * 2, ethers.parseEther('2000')],
+        ),
+      );
 
-    expect(await ve.ownerOf(1)).to.equal(ethers.ZeroAddress);
-    await ve.connect(accounts[2]).claimNative(bucketId, voter, end, stakedAmount, []);
-    expect(await ve.ownerOf(1)).to.equal(accounts[1].address);
-    const locked = await ve.locked(1);
-    expect(locked.amount).to.equal(stakedAmount);
-    expect(locked.isPermanent).to.equal(false);
-    expect(await ve.nativeTokenId(bucketId)).to.equal(1);
-    expect(await ve.tokenIdNative(1)).to.equal(bucketId);
+      root = ethers.keccak256(
+        Buffer.concat(
+          [Buffer.from(node1.substring(2), 'hex'), Buffer.from(node2.substring(2), 'hex')].sort(Buffer.compare),
+        ),
+      );
+    });
+
+    it('Native bucket Mapping', async function () {
+      await expect(ve.connect(accounts[1]).commitNativeRoot(root)).to.be.revertedWithCustomError(ve, 'NotTeam');
+      await ve.connect(accounts[0]).commitNativeRoot(root);
+      await ve.connect(accounts[0]).approveNativeRoot();
+
+      expect(await ve.nativeRoot()).to.equal(root);
+
+      expect(await ve.ownerOf(1)).to.equal(ethers.ZeroAddress);
+
+      await ve
+        .connect(accounts[2])
+        .claimNative(200, accounts[1].address, now + YEAR, ethers.parseEther('100'), [node2]);
+      expect(await ve.ownerOf(1)).to.equal(accounts[1].address);
+      const locked = await ve.locked(1);
+      expect(locked.amount).to.equal(ethers.parseEther('100'));
+      expect(locked.isPermanent).to.equal(false);
+      expect(await ve.nativeTokenId(200)).to.equal(1);
+      expect(await ve.tokenIdNative(1)).to.equal(200);
+    });
   });
 });
