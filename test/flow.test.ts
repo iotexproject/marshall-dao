ad6f09e87a76b7f5413d1aa59902d4a5f22d6af1
yongxinyao <yyxyong@163.com>
2024-07-02 17:04:31 +0800

    chore: fix lint

--- test/flow.test.ts
+++ test/flow.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import {address} from "hardhat/internal/core/config/config-validation";
+import { address } from 'hardhat/internal/core/config/config-validation';
 
 describe('Flow', function () {
   it('Vote flow', async function () {
@@ -8,13 +8,14 @@ describe('Flow', function () {
     const forwarder = await ethers.deployContract('Forwarder');
     const poolFactory = await ethers.deployContract('EmptyPoolFactory', []);
     const gaugeFactory = await ethers.deployContract('GaugeFactory', []);
-    const factoryRegistry = await ethers.deployContract('FactoryRegistry', [
-      poolFactory.target,
-      gaugeFactory.target,
-    ]);
+    const factoryRegistry = await ethers.deployContract('FactoryRegistry', [poolFactory.target, gaugeFactory.target]);
     const strategyManager = await ethers.deployContract('TestStrategyManager');
     await strategyManager.setShare(deployer.address, 100);
-    const voter = await ethers.deployContract('Voter', [forwarder.target, strategyManager.target, factoryRegistry.target]);
+    const voter = await ethers.deployContract('Voter', [
+      forwarder.target,
+      strategyManager.target,
+      factoryRegistry.target,
+    ]);
 
     const rewardsDistributor = await ethers.deployContract('RewardsDistributor', [strategyManager.target]);
     await rewardsDistributor.waitForDeployment();
@@ -26,7 +27,7 @@ describe('Flow', function () {
     await rewardsDistributor.setVault(vault.target);
 
     const token = await ethers.deployContract('TestToken', ['Test Token', 'TEST']);
-    await voter .createGauge(poolFactory.target, token.target);
+    await voter.createGauge(poolFactory.target, token.target);
 
     await voter.vote([token.target], [5000]);
   });
