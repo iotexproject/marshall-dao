a37f3c433115c6bccf4e1a21cb245fa6c1df2c7f
Xin <yyxyong@163.com>
2024-07-23 12:16:57 +0800

    tests: add test for deviceNFT gauge

---
+++ contracts/test/TestDeviceNFT.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IDeviceNFT} from "../interfaces/IDeviceNFT.sol";
+
+contract TestDeviceNFT is IDeviceNFT, ERC721 {
+  mapping(uint256 => uint256) public weightOf;
+
+  constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    _mint(msg.sender, 1);
+    _mint(msg.sender, 2);
+    _mint(msg.sender, 3);
+    weightOf[1] = 1 ether;
+    weightOf[2] = 2 ether;
+    weightOf[3] = 3 ether;
+  }
+
+  function weight(uint256 tokenId) external view returns (uint256) {
+    return weightOf[tokenId];
+  }
+
+  function setWeight(uint256 _tokenId, uint256 _weight) public {
+    weightOf[_tokenId] = _weight;
+  }
+}
--- test/TestIncentive.sol
+++ test/TestIncentive.sol
@@ -11,6 +11,8 @@ import {ProtocolTimeLibrary} from "../contracts/libraries/ProtocolTimeLibrary.so
 import {Incentives} from "../contracts/rewards/Incentive.sol";
 import {IIncentive} from "../contracts/interfaces/IIncentive.sol";
 import {TestStrategyManager} from "../contracts/test/TestStrategyManager.sol";
+import {DeviceGauge} from "../contracts/gauges/DeviceGauge.sol";
+import {TestDeviceNFT} from "../contracts/test/TestDeviceNFT.sol";
 
 contract TestIncentive is Test {
   address[] public rewardTokens;
@@ -21,6 +23,20 @@ contract TestIncentive is Test {
   Incentives public inti;
   ERC20Gauge public gauge;
   TestToken public lp;
+
+  /// device nft and gauge
+  DeviceGauge public deviceGauge;
+  TestDeviceNFT public dlp;
+  Incentives public dinti;
+
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) external returns (bytes4) {
+    return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+  }
 
   function setUp() external {
     for (uint256 i; i < 20; i++) {
@@ -38,6 +54,13 @@ contract TestIncentive is Test {
     vm.prank(address(voter));
     inti.setGauge(address(gauge));
     voter.reviveGauge(address(gauge));
+
+    dlp = new TestDeviceNFT("device nft", "device");
+    dinti = new Incentives(address(forwarder), address(voter), rewardTokens);
+    deviceGauge = new DeviceGauge(address(forwarder), address(dlp), address(voter), address(dinti));
+    vm.prank(address(voter));
+    dinti.setGauge(address(deviceGauge));
+    voter.reviveGauge(address(deviceGauge));
   }
 
   function test_setup_addRewardToken() external {
@@ -66,7 +89,7 @@ contract TestIncentive is Test {
     inti.setGauge(address(2));
   }
 
-  function test_deposit_withdraw() external {
+  function test_deposit_withdraw_ERC20() external {
     // 1. init lp state
     assertEq(10000 ether, lp.balanceOf(address(this)));
     lp.approve(address(gauge), 10000 ether);
@@ -90,6 +113,30 @@ contract TestIncentive is Test {
     assertEq(2 ether, inti.totalSupply());
   }
 
+  function test_deposit_withdraw_DeviceNFT() external {
+    // 1. init dlp state
+    dlp.approve(address(deviceGauge), 1);
+    dlp.approve(address(deviceGauge), 2);
+
+    // 2. deposit lp into gauge and callback to incentives
+    deviceGauge.deposit(1);
+    assertEq(1 ether, dinti.balanceOf(address(this)));
+    assertEq(1 ether, dinti.totalSupply());
+
+    // 3. again deposit to check increase
+    deviceGauge.deposit(2);
+    assertEq(3 ether, dinti.balanceOf(address(this)));
+    assertEq(3 ether, dinti.totalSupply());
+
+    // 4. rewardRate should be zero due to not notifyReward.
+    assertEq(0, dinti.rewardRate(rewardTokens[0]));
+
+    // 5. withdraw
+    deviceGauge.withdraw(1);
+    assertEq(2 ether, dinti.balanceOf(address(this)));
+    assertEq(2 ether, dinti.totalSupply());
+  }
+
   function test_notifyReward_Claim() external {
     // 1. base check
     vm.expectRevert(IIncentive.ZeroAmount.selector);
@@ -102,11 +149,30 @@ contract TestIncentive is Test {
     TestToken(rewardTokens[0]).approve(address(inti), 10000 ether);
     TestToken(rewardTokens[1]).approve(address(inti), 10000 ether);
     inti.notifyRewardAmount(rewardTokens[0], 1 ether);
+    inti.notifyRewardAmount(rewardTokens[1], 2 ether);
 
     uint256 timeUntilNext = ProtocolTimeLibrary.epochNext(block.timestamp) - block.timestamp;
     uint256 epochStart = ProtocolTimeLibrary.epochStart(block.timestamp);
     uint256 expectedRewardRate = 1 ether / timeUntilNext;
     assertEq(expectedRewardRate, inti.rewardRate(rewardTokens[0]));
     assertEq(expectedRewardRate, inti.rewardRateByEpoch(epochStart, rewardTokens[0]));
+    assertEq(2 ether / timeUntilNext, inti.rewardRate(rewardTokens[1]));
+    assertEq(2 ether / timeUntilNext, inti.rewardRateByEpoch(epochStart, rewardTokens[1]));
+    assertEq(block.timestamp, inti.lastUpdateTime(rewardTokens[0]));
+    assertEq(block.timestamp, inti.lastUpdateTime(rewardTokens[1]));
+
+    // 3. deposit and will claim
+    lp.approve(address(gauge), 10000 ether);
+    gauge.deposit(1 ether);
+    skip(7 days);
+    uint256 _escape = inti.lastTimeRewardApplicable(rewardTokens[0]) - inti.lastUpdateTime(rewardTokens[0]);
+    uint256 earned = inti.earned(address(this), rewardTokens[0]);
+    assertEq(earned, _escape * expectedRewardRate);
+    uint256 before = TestToken(rewardTokens[0]).balanceOf(address(this));
+    address[] memory tokens = new address[](1);
+    tokens[0] = rewardTokens[0];
+    inti.claimReward(tokens);
+    uint256 after_ = TestToken(rewardTokens[0]).balanceOf(address(this));
+    assertEq(after_ - before, earned);
   }
 }
